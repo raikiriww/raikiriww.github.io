18e937b38ef12706984397249f1a498ce29cd6b4
raikiriww <raikiriww@gmail.com>
2026-09-05 01:30:46 +0000

    chore(hn-daily): publish 2026-09-04

---
+++ hn-daily/site-data.js
@@ -0,0 +1,102 @@
+window.__HN_SITE_DATA__ = {
+  "subscribeLinks": [
+    {
+      "href": "./index.rss",
+      "label": "RSS feed"
+    },
+    {
+      "href": "./2026-09-04.html",
+      "label": "Latest page"
+    },
+    {
+      "href": "./index.html",
+      "label": "Archive home"
+    },
+    {
+      "href": "https://www.daemonology.net/hn-daily/",
+      "label": "Source page"
+    }
+  ],
+  "recentPosts": [
+    {
+      "href": "./2026-09-04.html",
+      "label": "Daily Hacker News for 2026-09-04"
+    },
+    {
+      "href": "./2026-09-03.html",
+      "label": "Daily Hacker News for 2026-09-03"
+    },
+    {
+      "href": "./2026-09-02.html",
+      "label": "Daily Hacker News for 2026-09-02"
+    },
+    {
+      "href": "./2026-09-01.html",
+      "label": "Daily Hacker News for 2026-09-01"
+    },
+    {
+      "href": "./2026-08-31.html",
+      "label": "Daily Hacker News for 2026-08-31"
+    },
+    {
+      "href": "./2026-08-30.html",
+      "label": "Daily Hacker News for 2026-08-30"
+    },
+    {
+      "href": "./2026-08-29.html",
+      "label": "Daily Hacker News for 2026-08-29"
+    },
+    {
+      "href": "./2026-08-28.html",
+      "label": "Daily Hacker News for 2026-08-28"
+    },
+    {
+      "href": "./2026-08-27.html",
+      "label": "Daily Hacker News for 2026-08-27"
+    },
+    {
+      "href": "./2026-08-26.html",
+      "label": "Daily Hacker News for 2026-08-26"
+    }
+  ],
+  "monthlyArchives": [
+    {
+      "href": "./2026-09.html",
+      "label": "September 2026"
+    },
+    {
+      "href": "./2026-08.html",
+      "label": "August 2026"
+    },
+    {
+      "href": "./2026-07.html",
+      "label": "July 2026"
+    },
+    {
+      "href": "./2026-06.html",
+      "label": "June 2026"
+    },
+    {
+      "href": "./2026-05.html",
+      "label": "May 2026"
+    },
+    {
+      "href": "./2026-04.html",
+      "label": "April 2026"
+    },
+    {
+      "href": "./2026-03.html",
+      "label": "March 2026"
+    },
+    {
+      "href": "./2026-02.html",
+      "label": "February 2026"
+    }
+  ],
+  "yearlyArchives": [
+    {
+      "href": "./2026.html",
+      "label": "2026"
+    }
+  ]
+};
